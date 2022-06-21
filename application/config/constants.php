fc0d0f21237bc7f2e44cbd2743f2755dfdad0f25
정남호 <drdo0110@gmail.com>
2022-06-21 12:34:11 +0900

    test

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,7 +83,7 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
-echo realpath(dirname(__FILE__));
+
 if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
     define('APP_PATH', '');
 } else if ($_SERVER['SERVER_ADDR'] == '172.31.43.153') {
