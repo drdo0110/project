f41ee0179f0f4e3a94e98384201353b29e1e94ad
정남호 <drdo0110@gmail.com>
2022-06-21 12:34:50 +0900

    test

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-print_r(realpath(dirname(__FILE__)));
+
 if(realpath(dirname(__FILE__)) == '/home/nhjung/public_html') {
     define('ENVIRONMENT', 'production');
 } else {
