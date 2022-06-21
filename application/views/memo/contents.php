bb0f016db36706a76ffa7d0f1cd6533773a8542d
정남호 <drdo0110@gmail.com>
2022-06-21 11:58:43 +0900

    test

--- application/views/memo/contents.php
+++ application/views/memo/contents.php
@@ -1,7 +1,6 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
